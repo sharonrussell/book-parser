0b344ea63fd051245e88090386fe721a109dd1d5
Amie <amie.yau@comparethemarket.com>
2016-07-27 14:30:02 +0100

    ayau created a hash from array and counted occurances

--- spec/book_parser_spec.rb
+++ spec/book_parser_spec.rb
@@ -9,4 +9,14 @@ describe BookParser do
 		book_parser = BookParser.new("./src/book.txt")
 		expect(book_parser.Words).to eq("The cat in the hat")
 	end
+
+	it 'takes the words and returns them in an array' do
+		book_parser = BookParser.new("./src/book.txt")
+		expect(book_parser.Convert_Words_To_Array("The cat in the hat")).to eq(["The", "cat", "in", "the", "hat"])
+	end
+
+	it 'takes the words in the array puts them inn a hash and counts the occrances of words' do
+		book_parser = BookParser.new("./src/book.txt")
+		expect(book_parser.Count_Occurance_of_Words(["The", "cat", "in", "the", "hat"])).to eq("the"=>2, "cat"=>1, "in"=>1, "hat"=>1)
+	end
 end
--- src/book_parser.rb
+++ src/book_parser.rb
@@ -1,16 +1,27 @@
 class BookParser
 
-def initialize(file_path)
-	@file_path = file_path
-end
+  def initialize(file_path)
+	  @file_path = file_path
+  end
 
-def Words
-	data = ''
-	f = File.open(@file_path,"r")
-f.each_line do |line|
-	data += line.chomp
-	return data
-	#data = data.split(" ")
-end
-end
+  def Words
+	  data = ''
+	  f = File.open(@file_path,"r")
+    f.each_line do |line|
+	    data += line.chomp
+	    return data
+    end
+  end
+
+	def Convert_Words_To_Array(data)
+		data.split(" ")
+		#parse in the data returned by Words and convert it to an array
+	end
+
+	def Count_Occurance_of_Words(array)
+    #I want to create a new hash and add my array words to it as a key and the number of times they appear as a value
+    words = Hash.new(0)
+    array.each {|x|words[x.downcase]+= 1}
+    return words
+		end
 end
